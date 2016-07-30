395d605d03226d38adf62f14d8de7fab6731e331
Diegobalay1 <dlolhd@gmail.com>
2016-07-30 21:15:45 +0200

    Initial commit

---
+++ app/src/main/java/es/diego3l/flash_75/MainActivity.java
@@ -0,0 +1,31 @@
+package es.diego3l.flash_75;
+
+import android.hardware.Camera;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button boton;
+    Camera camera;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        boton = (Button) findViewById(R.id.boton);
+        boton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                camera = Camera.open();
+                Camera.Parameters parameters = camera.getParameters();
+                parameters.setFlashMode(Camera.Parameters.FLASH_MODE_TORCH);
+                camera.setParameters(parameters);
+                camera.startPreview();
+            }
+        });
+
+    }
+}
